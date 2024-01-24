7aabf8ab1d657314939064e1652292ad3416c01c
LeelaRohith <leelarohith10@gmail.com>
2024-01-24 09:35:24 +0530

    created add task method

--- src/main/java/com/tasklistApp/controllers/TaskListController.java
+++ src/main/java/com/tasklistApp/controllers/TaskListController.java
@@ -1,16 +1,38 @@
 package com.tasklistApp.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tasklistApp.data.Task;
 
 @Controller 
 public class TaskListController
 {
+	ArrayList<Task> tasks= new ArrayList<>();
+	
 	
 	@RequestMapping("/")
 	public String homepage()
 	{
 		System.out.println("Home page called");
 		return "homepage.jsp";
+	}
+	@PostMapping("/addTask")
+	public ModelAndView addTask(@RequestParam("newTaskInput") String name)
+	{
+		ModelAndView mv = new ModelAndView();
+		Task t = new Task();
+		t.setId(tasks.size()+1);
+		t.setName(name);
+		tasks.add(t);
+		System.out.println(tasks.get(0).getName());
+		mv.addObject("tasks",tasks);
+		mv.setViewName("tasklist.jsp");
+		return mv;
 	}
 }
